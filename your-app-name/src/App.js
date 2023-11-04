56c4ec0b92294fe0a7c4714eb17aaac8ea7a842b
Subham R B <srb966908@gmail.com>
2023-11-04 18:31:32 +0530

    New changes

--- your-app-name/src/App.js
+++ your-app-name/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import CodeExecutor from './Components/Code/codexec';
+import Navbar from './Components/Navbar/Navbar';
+import Body from './Components/Body/Body';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Body/>
-      
     </div>
   );
 }
